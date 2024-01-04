34d1bb1a8c4957c7be0db948c06e7de00e7d6721
AsHim Shrestha <ashimsth89@gmail.com>
2024-01-04 21:14:51 +0545

    finish UI of seaarchBox

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,6 @@ export default function RootLayout({
             <Header/>
             <Navbar/>
             <SearchBox/>
-
             {children}
         </Providers>
 
--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 
 const SearchBox = () => {
     return (
-        <div>
-            
-        </div>
+        <form className='flex max-w-6xl mx-auto justify-between items-center px-5'>
+            <input type="text" placeholder='Search for a movie...'
+                   className='w-full h-14 rounded-sm placeholder-gray-500 outline-none bg-transparent flex-1'/>
+            <button type='submit' className='text-amber-600 disabled:text-red-400'>Search</button>
+
+
+        </form>
     );
 };
 
