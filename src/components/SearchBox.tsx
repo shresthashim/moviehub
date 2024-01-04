f03cde3b14e55753dd429f2d472affb854755dc9
AsHim Shrestha <ashimsth89@gmail.com>
2024-01-04 21:16:50 +0545

    chore : add state hook in input

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+'use client'
+
+import React, {useState} from 'react';
+
 
 const SearchBox = () => {
+
+    const [input, setInput] = useState('')
+
     return (
         <form className='flex max-w-6xl mx-auto justify-between items-center px-5'>
-            <input type="text" placeholder='Search for a movie...'
-                   className='w-full h-14 rounded-sm placeholder-gray-500 outline-none bg-transparent flex-1'/>
+            <input onChange={(e) => setInput(e.target.value)} type="text" placeholder='Search for a movie...'
+                   className='w-full h-14 rounded-sm placeholder-gray-500 outline-none bg-transparent flex-1'
+                   value={input}/>
             <button type='submit' className='text-amber-600 disabled:text-red-400'>Search</button>
 
 
