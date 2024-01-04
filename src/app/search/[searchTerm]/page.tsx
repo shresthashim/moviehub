51791c054c251ac11fba9936f4e21ea0b4d40ff3
AsHim Shrestha <ashimsth89@gmail.com>
2024-01-04 21:45:54 +0545

    fetch data from api for searchTerm/page

--- src/app/search/[searchTerm]/page.tsx
+++ src/app/search/[searchTerm]/page.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
 
-const SearchPage = () => {
+// @ts-ignore
+const SearchPage = async ({params}) => {
+
+    const searchTerm = params.searchTerm;
+
+    const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${searchTerm}&language=en-US&include_adult=false`);
+
+    if (!res.ok) {
+        throw new Error('Something went wrong');
+    }
+
+    const data = await res.json();
+    const results = data.results;
+
+
     return (
         <div>
             haha
