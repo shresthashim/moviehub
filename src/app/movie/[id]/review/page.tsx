60a1f9c6cf6b1d33adf4816f631e77cfe35a3236
AsHim Shrestha <ashimsth89@gmail.com>
2024-01-06 20:56:12 +0545

    style : reviews grid

--- src/app/movie/[id]/review/page.tsx
+++ src/app/movie/[id]/review/page.tsx
@@ -22,18 +22,18 @@ const ReviewPage: React.FC<ReviewPageProps> = async ({params}) => {
     console.log(review);
 
     return (
-        <div>
-            <h2 className='text-lg mb-3 font-bold'>Reviews</h2>
-            <div className='flex flex-col md:flex-row items-center content-center max-w-6xl mx-auto md:space-x-6'>
+        <div className="container mx-auto mt-8">
+            <h2 className="text-2xl mb-6 font-bold">Movie Reviews</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                 {review.results.map((result: Review) => (
-                    <div className='p-2' key={result.id}>
-                        <h2 className='text-lg mb-3 font-bold'>{result.author}</h2>
-                        <p className='text-lg mb-3'>
-                            <span className='font-semibold mr-1'>Content :</span>
+                    <div className="bg-gray-100 dark:bg-gray-600 p-6 rounded-lg shadow-md" key={result.id}>
+                        <h2 className="text-xl mb-3 font-bold">{result.author}</h2>
+                        <p className="text-lg mb-3">
+                            <span className="font-semibold mr-1 ">Content:</span>
                             {result.content}
                         </p>
-                        <p className='mb-3'>
-                            <span className='font-semibold mr-1'>Date :</span>
+                        <p className="text-sm">
+                            <span className="font-semibold mr-1">Date:</span>
                             {result.created_at}
                         </p>
                     </div>
